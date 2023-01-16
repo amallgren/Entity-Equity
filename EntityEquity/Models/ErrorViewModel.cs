065d1c56cce9770591f718073ce4da1cae13129b
abmalllgren <96656360+abmalllgren@users.noreply.github.com>
2023-01-15 20:21:04 -0500

    Add project files.

---
+++ EntityEquity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EntityEquity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
